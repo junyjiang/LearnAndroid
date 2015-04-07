c612de44ebca832dff073e2ad55ba9ec21e4a1f1
jiangchao3 <jiangchao3@lenovo.com>
2015-04-07 19:43:09 +0800

    task学习01

--- learning/gen/com/junyjiang/learning/R.java
+++ learning/gen/com/junyjiang/learning/R.java
@@ -26,12 +26,15 @@ public final class R {
     public static final class id {
         public static final int EditText01=0x7f080000;
         public static final int TextView01=0x7f080002;
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080005;
         public static final int btClick=0x7f080001;
+        public static final int btOther=0x7f080003;
+        public static final int btSend=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_new=0x7f030001;
+        public static final int other_activity=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -41,8 +44,10 @@ public final class R {
         public static final int abort=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int call_other=0x7f050006;
         public static final int exit=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int send=0x7f050007;
     }
     public static final class style {
         /** 
--- learning/src/com/junyjiang/learning/NewActivity.java
+++ learning/src/com/junyjiang/learning/NewActivity.java
@@ -4,22 +4,37 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NewActivity extends Activity{
 	private TextView TextView01;
+	private Button btOther;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new);
+		btOther = (Button)findViewById(R.id.btOther);
+		btOther.setText(R.string.call_other);
 		TextView01 = (TextView)findViewById(R.id.TextView01);
 		//获取intent对象中的值
 		Intent intent = getIntent();
 		String text1 = intent.getStringExtra("text01");
 		Log.e("junyjiang", text1+"-------------");
 		TextView01.setText(text1);
-		
+		btOther.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent= new Intent();
+				intent.setClass(NewActivity.this, OtherActivity.class);
+				startActivity(intent);
+				
+			}
+		});
 	}
 	
 
