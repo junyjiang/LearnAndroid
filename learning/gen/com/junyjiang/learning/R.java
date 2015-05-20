d32c8225d14f9c811a3bcd20a6972739fd7812d0
jiangchao3 <jiangchao3@lenovo.com>
2015-05-20 17:14:23 +0800

    listview

--- learning/gen/com/junyjiang/learning/R.java
+++ learning/gen/com/junyjiang/learning/R.java
@@ -26,21 +26,32 @@ public final class R {
     public static final class id {
         public static final int EditText01=0x7f080000;
         public static final int TextView01=0x7f080002;
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f080012;
         public static final int btClick=0x7f080001;
         public static final int btOther=0x7f080003;
-        public static final int btSend=0x7f080004;
-        public static final int femaleButton=0x7f080008;
-        public static final int genderGroup=0x7f080007;
-        public static final int maleButton=0x7f080009;
-        public static final int radiotextview=0x7f080006;
-        public static final int startRadioActivity=0x7f080005;
+        public static final int btSend=0x7f080005;
+        public static final int femaleButton=0x7f08000c;
+        public static final int firstProgressBar=0x7f080007;
+        public static final int genderGroup=0x7f08000b;
+        public static final int listviewLinearLayout=0x7f080004;
+        public static final int maleButton=0x7f08000d;
+        public static final int progressbt=0x7f080009;
+        public static final int radiotextview=0x7f08000a;
+        public static final int read=0x7f080010;
+        public static final int run=0x7f08000f;
+        public static final int secondProgressBar=0x7f080008;
+        public static final int startPro=0x7f080011;
+        public static final int startRadioActivity=0x7f080006;
+        public static final int swim=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_new=0x7f030001;
-        public static final int other_activity=0x7f030002;
-        public static final int radio=0x7f030003;
+        public static final int listviewmain=0x7f030002;
+        public static final int other_activity=0x7f030003;
+        public static final int progressbar=0x7f030004;
+        public static final int radio=0x7f030005;
+        public static final int user=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -53,10 +64,16 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int call_other=0x7f050007;
         public static final int exit=0x7f050005;
-        public static final int female=0x7f05000a;
+        public static final int female=0x7f05000b;
         public static final int hello_world=0x7f050002;
-        public static final int male=0x7f050009;
+        public static final int list=0x7f050010;
+        public static final int listviewLinearLayout=0x7f05000f;
+        public static final int male=0x7f05000a;
+        public static final int radio=0x7f050009;
+        public static final int read=0x7f05000e;
+        public static final int run=0x7f05000d;
         public static final int send=0x7f050008;
+        public static final int swim=0x7f05000c;
     }
     public static final class style {
         /** 
---
+++ learning/src/com/junyjiang/learning/ListViewTest.java
@@ -0,0 +1,5 @@
+package com.junyjiang.learning;
+
+public class ListViewTest {
+
+}
